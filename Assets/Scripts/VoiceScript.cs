ae0ceb15cba7aeb27792266686ad5bea69aaf1a5
Devin Zhang <devinzhang415@gmail.com>
2024-06-20 23:17:05 -0400

    transcription is not continuous, workaround with 300 s limit

--- Assets/Scripts/VoiceScript.cs
+++ Assets/Scripts/VoiceScript.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Oculus.Voice;
 using Oculus.Voice.Dictation;
+using Meta.WitAi.Data;
 
 public class VoiceScript : MonoBehaviour
 {
-    public AppDictationExperience dictationExperience;
+    [SerializeField]
+    private AppDictationExperience dictationExperience;
 
     // Start is called before the first frame update
     void Start()
     {
         dictationExperience.Activate();
+
+        // dictationExperience.AudioEvents.OnMicStoppedListening.AddListener(() => {
+        //     dictationExperience.Activate();
+        // });
     }
 
     // Update is called once per frame
